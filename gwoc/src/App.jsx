1a164140cb8f84e7ded3358795f7d428bd3b3a1d
Diya5772 <your.email@example.com>
2025-02-06 23:23:04 +0530

    routing

--- gwoc/src/App.jsx
+++ gwoc/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Colllection from './pages/Colllection'
+import AboutUs from './pages/AboutUs'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Blogs from './pages/Blogs'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Cart from './pages/cart'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/Collection' element={<Colllection/>}/>
+<Route path='/AboutUs' element={<AboutUs/>}/>
+
+<Route path='/Blogs' element={<Blogs/>}/>
+
+<Route path='/Collection' element={<Collection/>}/>
+<Route path='/Product/:ProductId' element={<Product/>}/>
+<Route path='/Cart' element={<Cart/>}/>
+<Route path='/Login' element={<Login/>}/>
+<Route path='/PlaceOrder' element={<PlaceOrder/>}/>
+<Route path='/Orders' element={<Orders/>}/>
+
+
+
+      </Routes>
+    </div>
   )
 }
 
---
+++ gwoc/src/pages/Colllection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Colllection = () => {
+  return (
+    <div>Colllection</div>
+  )
+}
+
+export default Colllection
