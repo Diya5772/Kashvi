2b46fe486298c2cc24194e02ff0ffb3c40bbf304
sanjhi-1706 <144031617+sanjhi-1706@users.noreply.github.com>
2025-02-07 13:08:50 +0530

    home

---
+++ gwoc/src/components/NewsletterBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+  return (
+    <div className='text-center'>
+        <p>Subscribe </p>
+      
+    </div>
+  )
+}
+
+export default NewsletterBox
--- gwoc/src/pages/Home.jsx
+++ gwoc/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <LatestCollection/>
+      <OurPolicy/>
+    </div>
   )
 }
 
